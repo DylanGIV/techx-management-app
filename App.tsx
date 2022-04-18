d518f3d90ea8134d3ea057bd1132a2f17eaad09b
dylangiv <dylangiv123@gmail.com>
2022-04-18 15:34:09 -0700

    Added login screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        label="Username"
+        value={username}
+        onChangeText={text => setUsername(text)}
+        returnKeyType="next"
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+      />
+      <Button  mode='contained' >
+        Log in
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
