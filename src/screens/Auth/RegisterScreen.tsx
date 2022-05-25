36a80ea75292a5fcd7ef048daab758b9d2f5654a
Dominic Fanucchi <dominic_fanucchi@yahoo.com>
2022-05-24 22:17:31 -0700

    styled RegisterScreen

--- src/screens/Auth/RegisterScreen.tsx
+++ src/screens/Auth/RegisterScreen.tsx
@@ -209,65 +209,75 @@ const RegisterScreen = (props : LoginProps) => {
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    text: {
-      fontSize: 24,
-      textAlign: 'center',
-    },
-    textInput: {
-      marginHorizontal: 8,
-      marginBottom: 14
-    },
-    inputLoginContainer: {
-      flex: 0.95,
-      justifyContent: 'center',
-      flexDirection: 'row',
-    },
-    inputContainer: {
-      flex: 0.166,
-    },
-    inputCenter: {
-      flex: 0.8,
-    },
-    inputBorder: {
-      flex: 0.1
-    },
-    registerContainer: {
-      flex: 0.0833,
-      justifyContent: 'flex-end',
-      alignItems: 'center'
-    },
-    touchableContainer: {
-      flex: 1,
-    },
-    wrapperView: {
-      flex: 1,
-    },
-    nameInputContainer: {
-      flex: 0.166,
-      flexDirection: 'row',
-      flexGrow: 0.166
-    },
-    individualNameContainer: {
-      flex: 1,
-    },
-    loadingIndicator: {
-      position: 'absolute', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      backgroundColor: 'black', 
-      opacity: 0.5, 
-      left: 0, 
-      right: 0, 
-      top: 0, 
-      bottom: 0,
-    },
-    infoTextWrapper: {
-      flex: 0.166,
-    }
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: '#7ED957'
+  },
+  textInput: {
+    marginHorizontal: 8,
+    marginBottom: 14,
+    backgroundColor: '#FFFFFF',
+  },
+  inputLoginContainer: {
+    flex: 0.95,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    color: '#7ED957',
+  },
+  inputContainer: {
+    flex: 0.166,
+    color: '#7ED957',
+  },
+  inputCenter: {
+    flex: 0.8,
+    color: '#7ED957',
+  },
+  inputBorder: {
+    flex: 0.1
+  },
+  registerContainer: {
+    flex: 0.0833,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#8A95A6',
+  },
+  touchableContainer: {
+    flex: 1,
+    backgroundColor: '#8A95A6',
+  },
+  wrapperView: {
+    flex: 1,
+    backgroundColor: '#8A95A6',
+  },
+  nameInputContainer: {
+    flex: 0.166,
+    flexDirection: 'row',
+    flexGrow: 0.166,
+    color: 'black',
+  },
+  individualNameContainer: {
+    flex: 1,
+    backgroundColor: '#8A95A6',
+  },
+  loadingIndicator: {
+    position: 'absolute', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: 'black', 
+    opacity: 0.5, 
+    left: 0, 
+    right: 0, 
+    top: 0, 
+    bottom: 0,
+  },
+  infoTextWrapper: {
+    flex: 0.166,
+  }
 
-  });  
+});  
 
 export default RegisterScreen;
